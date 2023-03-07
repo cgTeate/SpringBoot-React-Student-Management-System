3ef8be0b2643e0de148efeb5b7a71893e01ede28
Calvin Teater <laughingglint3@gmail.com>
2023-03-07 13:01:19 -0600

    configured and installed antd, unfetch, and axios,  Renamed api file to clientjs

--- src/frontend/src/pages/_app.js
+++ src/frontend/src/pages/_app.js
@@ -1,4 +1,5 @@
 import '@/styles/globals.css'
+import 'antd/dist/antd.css';
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />
---
+++ src/frontend/src/pages/api/client.js
@@ -0,0 +1,6 @@
+import axios from 'axios';
+import fetch from 'unfetch';
+// const url = process.env.NEXT_PUBLIC_SPRINGBOOT_API_URL
+
+
+
