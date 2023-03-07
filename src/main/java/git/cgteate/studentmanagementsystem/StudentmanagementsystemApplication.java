74b51d66361e963df6688ca89bca7cfc566fa22c
Calvin Teater <laughingglint3@gmail.com>
2023-03-07 10:48:52 -0600

    Spring Boot Setup

---
+++ src/main/java/git/cgteate/studentmanagementsystem/StudentmanagementsystemApplication.java
@@ -0,0 +1,13 @@
+package git.cgteate.studentmanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentmanagementsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentmanagementsystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/git/cgteate/studentmanagementsystem/StudentmanagementsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package git.cgteate.studentmanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentmanagementsystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
