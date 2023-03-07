51001cc467b2887f312cae298a46b67a7e7c160e
Calvin Teater <laughingglint3@gmail.com>
2023-03-07 13:26:44 -0600

    changed api method to axios becaus eunfetch will not work, added antd components

--- src/frontend/src/pages/api/client.js
+++ src/frontend/src/pages/api/client.js
@@ -1,6 +1,18 @@
 import axios from 'axios';
-import fetch from 'unfetch';
+// import fetch from 'unfetch';
 // const url = process.env.NEXT_PUBLIC_SPRINGBOOT_API_URL
 
+//method to return all students
+const checkStatus = response => {
+  if (response.status >= 200 && response.status < 300) {
+    return response;
+  }
+  // convert non-2xx HTTP responses into errors:
+  const error = new Error(response.statusText);
+  error.response = response;
+  throw error;
+};
 
-
+export const getAllStudents = () =>
+  axios.get("http://localhost:8080/api/v1/students")
+    .then(checkStatus);
--- src/frontend/src/pages/index.js
+++ src/frontend/src/pages/index.js
@@ -1,13 +1,23 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
+import { getAllStudents } from './api/client'
+import {Button, Radio} from 'antd'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <div className="App">
-      
-    </div>
+    <>
+      <div className="App">
+          <Button type="primary">Hello</Button>
+          <Radio.Group value='large'>
+            <Radio.Button value="large">Large</Radio.Button>
+            <Radio.Button value="default">Default</Radio.Button>
+            <Radio.Button value="small">Small</Radio.Button>
+          </Radio.Group>
+      </div>
+    </>
+    
   )
 }
