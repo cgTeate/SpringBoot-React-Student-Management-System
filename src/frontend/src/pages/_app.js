104abac4662184029b40b73c8f1cd49c63f4a166
Calvin Teater <laughingglint3@gmail.com>
2023-03-07 18:00:10 -0600

    downgraded to working deprecated antd dependencies, added antd layout for index page and antd css styles

--- src/frontend/src/pages/_app.js
+++ src/frontend/src/pages/_app.js
@@ -1,5 +1,5 @@
 import '@/styles/globals.css'
-import 'antd/dist/reset.css';
+import 'antd/dist/antd.css';
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />
--- src/frontend/src/pages/index.js
+++ src/frontend/src/pages/index.js
@@ -3,27 +3,78 @@ import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import { getAllStudents } from './api/client'
 import { useEffect, useState } from 'react'
+import { Layout, Menu, Breadcrumb } from 'antd';
+import {
+    DesktopOutlined,
+    PieChartOutlined,
+    FileOutlined,
+    TeamOutlined,
+    UserOutlined,
+} from '@ant-design/icons';
+
+const { Header, Content, Footer, Sider } = Layout;
+const { SubMenu } = Menu;
+
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
-
+  const [collapsed, setCollapsed] = useState(false);
   const [students, setStudents] = useState([]);
 
   const fetchStudents = () => {
     getAllStudents()
-    .then(res => setStudents(res.data))
-}
+      .then(res => setStudents(res.data))
+  }
 
-useEffect(()=>{
-  console.log("component is mounted");
-  fetchStudents();
- }, []);
+  useEffect(() => {
+    console.log("component is mounted");
+    fetchStudents();
+  }, []);
+
+  if (students.length <= 0) {
+    return <div>no data</div>;
+  }
 
   return (
-    <>
-      <h1>{students.length}</h1>
-    </>
-    
-  )
+    <Layout style={{ minHeight: '100vh' }}>
+    <Sider collapsible collapsed={collapsed}
+           onCollapse={setCollapsed}>
+        <div className="logo" />
+        <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline">
+            <Menu.Item key="1" icon={<PieChartOutlined />}>
+                Option 1
+            </Menu.Item>
+            <Menu.Item key="2" icon={<DesktopOutlined />}>
+                Option 2
+            </Menu.Item>
+            <SubMenu key="sub1" icon={<UserOutlined />} title="User">
+                <Menu.Item key="3">Tom</Menu.Item>
+                <Menu.Item key="4">Bill</Menu.Item>
+                <Menu.Item key="5">Alex</Menu.Item>
+            </SubMenu>
+            <SubMenu key="sub2" icon={<TeamOutlined />} title="Team">
+                <Menu.Item key="6">Team 1</Menu.Item>
+                <Menu.Item key="8">Team 2</Menu.Item>
+            </SubMenu>
+            <Menu.Item key="9" icon={<FileOutlined />}>
+                Files
+            </Menu.Item>
+        </Menu>
+    </Sider>
+    <Layout className="site-layout">
+        <Header className="site-layout-background" style={{ padding: 0 }} />
+        <Content style={{ margin: '0 16px' }}>
+            <Breadcrumb style={{ margin: '16px 0' }}>
+                <Breadcrumb.Item>User</Breadcrumb.Item>
+                <Breadcrumb.Item>Bill</Breadcrumb.Item>
+            </Breadcrumb>
+            <div className="site-layout-background" style={{ padding: 24, minHeight: 360 }}>
+                Bill is a cat.
+            </div>
+        </Content>
+        <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
+    </Layout>
+</Layout>
+  );
 }
