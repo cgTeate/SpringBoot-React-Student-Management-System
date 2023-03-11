4a35232d7e0f6b5463c5c7b9405e36dc25f81edf
Calvin Teater <laughingglint3@gmail.com>
2023-03-10 20:14:55 -0600

    implemented n tier architecture layers to interact with database with Spring JPA

--- src/main/java/git/cgteate/studentmanagementsystem/student/StudentController.java
+++ src/main/java/git/cgteate/studentmanagementsystem/student/StudentController.java
@@ -1,5 +1,6 @@
 package git.cgteate.studentmanagementsystem.student;
 
+import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,23 +11,14 @@ import java.util.List;
 
 @RestController
 @RequestMapping(path = "api/v1/students")
+@AllArgsConstructor
 @CrossOrigin("*")
 public class StudentController {
 
+    private final StudentService studentService;
+
     @GetMapping
     public List<Student> getAllStudents() {
-        List<Student> students = Arrays.asList(
-                new Student(
-                        1L,
-                        "Emma",
-                        "emma@washburn.edu",
-                        Gender.FEMALE),
-                new Student(
-                        2L,
-                        "Calvin",
-                        "calvin@washburn.edu",
-                        Gender.MALE)
-        );
-        return students;
+        return studentService.getAllStudents();
     }
 }
