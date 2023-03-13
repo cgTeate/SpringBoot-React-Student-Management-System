4b47df0463b08e9f8201b0a9f0fccf824ba714b4
Calvin Teater <laughingglint3@gmail.com>
2023-03-12 19:26:38 -0500

    implement delete functionality for the frontend and backend, state management used to handle reload of students on table

--- src/main/java/git/cgteate/studentmanagementsystem/student/StudentController.java
+++ src/main/java/git/cgteate/studentmanagementsystem/student/StudentController.java
@@ -2,7 +2,9 @@ package git.cgteate.studentmanagementsystem.student;
 
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,14 +20,20 @@ import java.util.List;
 public class StudentController {
 
     private final StudentService studentService;
-
+    //get all students
     @GetMapping
     public List<Student> getAllStudents() {
         return studentService.getAllStudents();
     }
-
+    //add a student
     @PostMapping
     public void addStudent(@RequestBody Student student) {
         studentService.addStudent(student);
+    }
+    //delete a student
+    @DeleteMapping(path = "{studentId}")
+    public void deleteStudent(
+            @PathVariable("studentId") Long studentId) {
+        studentService.deleteStudent(studentId);
     }
 }
--- src/main/java/git/cgteate/studentmanagementsystem/student/StudentService.java
+++ src/main/java/git/cgteate/studentmanagementsystem/student/StudentService.java
@@ -19,4 +19,8 @@ public class StudentService {
         // TODO: check if email is taken
         studentRepository.save(student);
     }
+    public void deleteStudent(Long studentId) {
+         // TODO: check if student exists
+        studentRepository.deleteById(studentId);
+    }
 }
