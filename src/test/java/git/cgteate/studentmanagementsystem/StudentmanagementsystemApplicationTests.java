6e70b830d5e074f55c85719a0ff7cca253ad6123
Calvin Teater <laughingglint3@gmail.com>
2023-03-13 13:05:48 -0500

    make this test fail

--- src/test/java/git/cgteate/studentmanagementsystem/StudentmanagementsystemApplicationTests.java
+++ src/test/java/git/cgteate/studentmanagementsystem/StudentmanagementsystemApplicationTests.java
@@ -1,5 +1,6 @@
 package git.cgteate.studentmanagementsystem;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +9,7 @@ class StudentmanagementsystemApplicationTests {
 
 	@Test
 	void contextLoads() {
+		Assertions.fail("Oops test failed");
 	}
 
 }
