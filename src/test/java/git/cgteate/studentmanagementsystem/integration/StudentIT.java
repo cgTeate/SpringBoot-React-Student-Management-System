cc4e21ab001d290b3ee6fc525c5cb76fa823a4b4
Calvin Teater <laughingglint3@gmail.com>
2023-03-16 19:36:42 -0500

    setup for integration testing with failsafe, also added driver datasource for other application.properties files

---
+++ src/test/java/git/cgteate/studentmanagementsystem/integration/StudentIT.java
@@ -0,0 +1,17 @@
+package git.cgteate.studentmanagementsystem.integration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+
+@SpringBootTest
+@TestPropertySource(
+        locations = "classpath:application-it.properties"
+)
+public class StudentIT {
+
+    @Test
+    void name(){
+
+    }
+}
